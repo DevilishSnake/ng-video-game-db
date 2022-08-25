273f3a3dacc1b7e5861feb9f75badb2e1bb96d81
DevilishSnake <joanleonelcortes@hotmail.com>
2022-08-25 15:51:56 -0300

    Made advances in 'Home' component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-//import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
@@ -23,7 +23,7 @@ import { HomeComponent } from './components/home/home.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    //AppRoutingModule,
+    AppRoutingModule,
     HttpClientModule,
     FormsModule,
     GaugeModule.forRoot(),
